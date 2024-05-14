6e05f6c8c416658647a699495226df443732b9f1
mmmeqqq <bew.bussayakorn@gmail.com>
2024-05-14 11:57:26 +0700

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+let dayNumber = 6;
+if(dayNumber == 0 || dayNumber == 6) {
+    console.log('It is the weekend!');
+} else if (dayNumber == 1 || dayNumber == 2 ||dayNumber == 3 || dayNumber == 4 || dayNumber == 5) {
+    console.log('It is the weekday!');
+} else{
+    console.log('Invalid day number')
+}
